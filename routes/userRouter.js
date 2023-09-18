1c6622a303b07016a708a39242b22a1948d003e4
RajPratapYadav <69375631+RajPratapYadav@users.noreply.github.com>
2023-09-18 15:05:32 +0530

    Update userRouter.js

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const UserModel = require('../models/user');
 const passport = require('passport');
 
